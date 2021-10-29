9fc188fa56b5fcabcece2e3d56bcaacacf9a3ea0
Andrey Malyshev <elvin.nnov@gmail.com>
2021-10-30 00:34:35 +0300

    Fix launch_rpc.sh to work with bash only env

--- scripts/launch_rpc.sh
+++ scripts/launch_rpc.sh
@@ -1,22 +1,12 @@
 #!/bin/bash
-function run() {
-    device_id=$1
-        echo "Starting rpc server on adb device: $device_id"
-    preamble='
-        spawn adb -s'
-        body='shell
-        expect "#"
-    set cmd "cd /data/local/tmp/; LD_LIBRARY_PATH=/data/local/tmp /data/local/tmp/tvm_rpc server --tracker=IP_ADDRESS:PORT --key=\"your_key_here\""
-        send $cmd
-        send "\r"
-        interact
-        '
-        expect -c "$preamble $device_id $body"
-}
 
 while true
 do
-        run $1
-        echo "RPC server died; restarting..." >&2
-        sleep 20
+   preamble='adb -s'
+    body='shell cd /data/local/tmp/tvm_rpc; LD_LIBRARY_PATH=/data/local/tmp/tvm_rpc /data/local/tmp/tvm_rpc/tvm_rpc server --host=0.0.0.0 --port=9090 --port-end=9091 --tracker=192.168.1.57:9190 --key=android
+        '
+        echo $preamble $1 $body
+        $preamble $1 $body
+
+    sleep 20
 done
