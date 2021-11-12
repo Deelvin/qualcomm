333525e31e3d2d719c854de3cf72fc88cb27788c
Egor Churaev <egor.churaev@gmail.com>
2021-11-12 17:03:00 +0300

    [OpenCLRunner] Add simple reproducer for problem with intrinsics

--- OpenCLRunner/app/src/main/cpp/openclrunner.cpp
+++ OpenCLRunner/app/src/main/cpp/openclrunner.cpp
@@ -2,7 +2,7 @@
 #include <string>
 
 #include "implementations/vector_add.h"
-#include "implementations/tvm_resnet50v2_conv_kernel.h"
+#include "implementations/simple_mad.h"
 
 extern "C" JNIEXPORT jstring JNICALL
 Java_com_deelvin_openclrunner_MainActivity_runOpenCL(
@@ -10,6 +10,6 @@ Java_com_deelvin_openclrunner_MainActivity_runOpenCL(
         jobject,
         jobject assetManager) {
     //std::string res = measureExecTime(vector_add, env, assetManager);
-    std::string res = measureExecTime(tvm_resnet50v2_conv_kernel, env, assetManager);
+    std::string res = measureExecTime(simple_mad, env, assetManager, 50);
     return env->NewStringUTF(res.c_str());
 }
