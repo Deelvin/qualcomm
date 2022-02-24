aa464eee6a8b4f7415faad2a12a01a05f74ab8bd
Andrey Malyshev <elvin.nnov@gmail.com>
2022-02-24 14:12:15 +0300

    remove legacy stuff

--- OpenCLCppRunner/implementations/tvm_mace_mobilenetv1_avg_pooling.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-#include <common_functions.h>
-
-ExecTime tvm_mace_mobilenetv1_avg_pooling_kernel();
-
--- OpenCLCppRunner/implementations/vector_add.h
+++
@@ -1,8 +0,0 @@
-#ifndef VECTOR_ADD_H
-#define VECTOR_ADD_H
-
-#include <common_functions.h>
-
-ExecTime vector_add();
-
-#endif // VECTOR_ADD_H
