5016bad37704cf9a9d5bec4cd9760238c5091f26
Egor Churaev <egor.churaev@gmail.com>
2022-06-21 16:04:03 +0300

    Updated script for working w/o wifi connection

--- launch_rpc.sh
+++ launch_rpc.sh
@@ -2,6 +2,7 @@
 
 RPC_KEY="android"
 PATH_PREFIX="/data/local/tmp"
+RPC_SERVER_PORT=9090
 
 help()
 {
@@ -12,12 +13,13 @@ help()
     echo "-t, --tracker           Tracker address (format: ip:port)"
     echo "-k, --key               Key of device in tracker (default: ${RPC_KEY})"
     echo "-p, --path              Path to directory with executable (default: ${PATH_PREFIX})"
+    echo "--rpc_server_port       Port of rpc_server on android (default: ${RPC_SERVER_PORT})"
     echo "-h, --help              Print this help message"
     exit 1
 }
 
 SHORT=d:,t:,k:,p:,h
-LONG=device:,tracker:,key:,path:,help
+LONG=device:,tracker:,key:,path:,rpc_server_port:,help
 OPTS=$(getopt -a -n launch_rpc --options $SHORT --longoptions $LONG -- "$@")
 
 VALID_ARGUMENTS=$# # Returns the count of arguments that are in short or long options
@@ -48,6 +50,10 @@ do
       PATH_PREFIX="$2"
       shift 2
       ;;
+    --rpc_server_port )
+      RPC_SERVER_PORT="$2"
+      shift 2
+      ;;
     -h | --help)
       help
       ;;
@@ -63,15 +69,28 @@ do
 done
 
 if [ "$TRACKER" == "" ] || [ "$DEVICE" == "" ]; then
-      help
+  help
+  exit 1
 fi
 
+tracker_port="$(cut -d':' -f2 <<<"$TRACKER")"
 while true
 do
-   preamble='adb -s'
-   body="shell cd ${PATH_PREFIX}; LD_LIBRARY_PATH=${PATH_PREFIX} ${PATH_PREFIX}/tvm_rpc server --host=0.0.0.0 --port=9090 --port-end=9091 --tracker=${TRACKER} --key=${RPC_KEY}"
-    echo $preamble ${DEVICE} $body
-    $preamble ${DEVICE} $body
+  preamble='adb -s'
+  body="shell cd ${PATH_PREFIX}; LD_LIBRARY_PATH=${PATH_PREFIX} ${PATH_PREFIX}/tvm_rpc server --host=0.0.0.0 --port=${RPC_SERVER_PORT} --tracker=${TRACKER} --key=${RPC_KEY}"
+  body_reverse="reverse tcp:${tracker_port} tcp:${tracker_port}"
+  echo $preamble ${DEVICE} $body_reverse
+  $preamble ${DEVICE} $body_reverse
 
-    sleep 20
+  for i in {0..3}; do
+    server_port=$((${RPC_SERVER_PORT} + ${i}))
+    body_forward="forward tcp:${server_port} tcp:${server_port}"
+    echo $preamble ${DEVICE} $body_forward
+    $preamble ${DEVICE} $body_forward
+  done
+
+  echo $preamble ${DEVICE} $body
+  $preamble ${DEVICE} $body
+
+  sleep 20
 done
